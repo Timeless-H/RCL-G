609cb14cf7716069b7ba369365788435e29778fa
Timeless-H <77699677+Timeless-H@users.noreply.github.com>
2024-04-25 06:50:43 +0000

    api_key issue fixed

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ client = Groq(api_key=api_key)
 chats = client.chat.completions.create(
     messages=[
         {
-            "role": "User",
-            "content": "what jobs can a phd in geomatics engineering with focus on geospatial vision get you?"
+            "role": "user",
+            "content": "what jobs can a fresh phd in geomatics engineering (with focus on geospatial vision) graduate with no industry work experience get?"
         }
     ],
     model="llama3-70b-8192"
